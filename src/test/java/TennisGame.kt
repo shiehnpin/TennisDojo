0a8911d180048302acd993b3edcb5ec4ffaab2b5
Hsieh Enping <shieh.npin@gmail.com>
2020-02-24 14:01:40 +0800

    Lester RRRRRRRRRR

--- src/test/java/TennisGame.kt
+++ src/test/java/TennisGame.kt
@@ -1,19 +1,24 @@
 class TennisGame(playerAName: String, playerBName: String) {
-    fun getResult(playerAScore: Int, playerBScore: Int): String {
+    fun getResult(playerAScore: Score, playerBScore: Score): String {
         if (playerAScore == playerBScore) {
             return when (playerAScore) {
-                0 -> "Love all"
-                1 -> "Fifteen all"
-                2 -> "Thirty all"
-                else -> ""
+                Score.LOVE -> "Love all"
+                Score.FIFTEEN -> "Fifteen all"
+                Score.THIRTY -> "Thirty all"
             }
         }
-        if (playerAScore == 1) {
-            if (playerBScore == 1) return "Fifteen all"
+        if (playerAScore == Score.FIFTEEN) {
+            if (playerBScore == Score.FIFTEEN) return "Fifteen all"
             else return "Fifteen/Love"
         }
         return "Love all"
     }
 
+}
+
+enum class Score(val score: Int, val msg: String) {
+    LOVE(0, "Love"),
+    FIFTEEN(1, "Fifteen"),
+    THIRTY(2, "Thirty")
 }
 
--- src/test/kotlin/TennisGameTest.kt
+++ src/test/kotlin/TennisGameTest.kt
@@ -6,28 +6,28 @@ class TennisGameTest{
     @Test
     fun `all_player_0`() {
         val tennisGame = TennisGame("Foo", "Bar")
-        val actual: Any = tennisGame.getResult(0, 0)
+        val actual: Any = tennisGame.getResult(Score.LOVE, Score.LOVE)
         Assert.assertEquals("Love all", actual)
     }
 
     @Test
     fun `Fifteen vs love`() {
         val tennisGame = TennisGame("Foo", "Bar")
-        val actual: Any = tennisGame.getResult(1, 0)
+        val actual: Any = tennisGame.getResult(Score.FIFTEEN, Score.LOVE)
         Assert.assertEquals("Fifteen/Love", actual)
     }
 
     @Test
     fun `Fifteen_all`() {
         val tennisGame = TennisGame("Foo", "Bar")
-        val actual: Any = tennisGame.getResult(1, 1)
+        val actual: Any = tennisGame.getResult(Score.FIFTEEN, Score.FIFTEEN)
         Assert.assertEquals("Fifteen all", actual)
     }
 
     @Test
     fun `Thirty_all`() {
         val tennisGame = TennisGame("Foo", "Bar")
-        val actual: Any = tennisGame.getResult(2, 2)
+        val actual: Any = tennisGame.getResult(Score.THIRTY, Score.THIRTY)
         Assert.assertEquals("Thirty all", actual)
     }
 
